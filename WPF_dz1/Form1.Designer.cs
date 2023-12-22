3c48363f1ea3ae8820cc7c59a8cca99735c57590
Vetal <vvvv@vvvv.vv>
2023-12-22 21:36:27 +0300

    added flowLayoutpanel to dz1 work1

--- WPF_dz1/Form1.Designer.cs
+++ WPF_dz1/Form1.Designer.cs
@@ -45,12 +45,14 @@
 			this.button15 = new System.Windows.Forms.Button();
 			this.button16 = new System.Windows.Forms.Button();
 			this.button17 = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
 			this.button1.ForeColor = System.Drawing.Color.Navy;
-			this.button1.Location = new System.Drawing.Point(11, 11);
+			this.button1.Location = new System.Drawing.Point(2, 2);
 			this.button1.Margin = new System.Windows.Forms.Padding(2);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
@@ -61,7 +63,7 @@
 			// button2
 			// 
 			this.button2.ForeColor = System.Drawing.Color.Blue;
-			this.button2.Location = new System.Drawing.Point(90, 11);
+			this.button2.Location = new System.Drawing.Point(81, 2);
 			this.button2.Margin = new System.Windows.Forms.Padding(2);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
@@ -72,7 +74,7 @@
 			// button3
 			// 
 			this.button3.ForeColor = System.Drawing.Color.Aqua;
-			this.button3.Location = new System.Drawing.Point(169, 11);
+			this.button3.Location = new System.Drawing.Point(160, 2);
 			this.button3.Margin = new System.Windows.Forms.Padding(2);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(75, 23);
@@ -83,7 +85,7 @@
 			// button4
 			// 
 			this.button4.ForeColor = System.Drawing.Color.Teal;
-			this.button4.Location = new System.Drawing.Point(248, 11);
+			this.button4.Location = new System.Drawing.Point(239, 2);
 			this.button4.Margin = new System.Windows.Forms.Padding(2);
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(75, 23);
@@ -94,7 +96,7 @@
 			// button5
 			// 
 			this.button5.ForeColor = System.Drawing.Color.Olive;
-			this.button5.Location = new System.Drawing.Point(327, 11);
+			this.button5.Location = new System.Drawing.Point(318, 2);
 			this.button5.Margin = new System.Windows.Forms.Padding(2);
 			this.button5.Name = "button5";
 			this.button5.Size = new System.Drawing.Size(75, 23);
@@ -105,7 +107,7 @@
 			// button6
 			// 
 			this.button6.ForeColor = System.Drawing.Color.Green;
-			this.button6.Location = new System.Drawing.Point(406, 11);
+			this.button6.Location = new System.Drawing.Point(397, 2);
 			this.button6.Margin = new System.Windows.Forms.Padding(2);
 			this.button6.Name = "button6";
 			this.button6.Size = new System.Drawing.Size(75, 23);
@@ -116,7 +118,7 @@
 			// button7
 			// 
 			this.button7.ForeColor = System.Drawing.Color.Lime;
-			this.button7.Location = new System.Drawing.Point(485, 11);
+			this.button7.Location = new System.Drawing.Point(476, 2);
 			this.button7.Margin = new System.Windows.Forms.Padding(2);
 			this.button7.Name = "button7";
 			this.button7.Size = new System.Drawing.Size(75, 23);
@@ -127,7 +129,7 @@
 			// button8
 			// 
 			this.button8.ForeColor = System.Drawing.Color.Yellow;
-			this.button8.Location = new System.Drawing.Point(564, 11);
+			this.button8.Location = new System.Drawing.Point(555, 2);
 			this.button8.Margin = new System.Windows.Forms.Padding(2);
 			this.button8.Name = "button8";
 			this.button8.Size = new System.Drawing.Size(75, 23);
@@ -138,7 +140,7 @@
 			// button9
 			// 
 			this.button9.ForeColor = System.Drawing.Color.Orange;
-			this.button9.Location = new System.Drawing.Point(643, 11);
+			this.button9.Location = new System.Drawing.Point(634, 2);
 			this.button9.Margin = new System.Windows.Forms.Padding(2);
 			this.button9.Name = "button9";
 			this.button9.Size = new System.Drawing.Size(75, 23);
@@ -149,7 +151,7 @@
 			// button10
 			// 
 			this.button10.ForeColor = System.Drawing.Color.Red;
-			this.button10.Location = new System.Drawing.Point(722, 11);
+			this.button10.Location = new System.Drawing.Point(713, 2);
 			this.button10.Margin = new System.Windows.Forms.Padding(2);
 			this.button10.Name = "button10";
 			this.button10.Size = new System.Drawing.Size(75, 23);
@@ -160,7 +162,7 @@
 			// button11
 			// 
 			this.button11.ForeColor = System.Drawing.Color.Maroon;
-			this.button11.Location = new System.Drawing.Point(11, 38);
+			this.button11.Location = new System.Drawing.Point(2, 29);
 			this.button11.Margin = new System.Windows.Forms.Padding(2);
 			this.button11.Name = "button11";
 			this.button11.Size = new System.Drawing.Size(75, 23);
@@ -171,7 +173,7 @@
 			// button12
 			// 
 			this.button12.ForeColor = System.Drawing.Color.Fuchsia;
-			this.button12.Location = new System.Drawing.Point(90, 38);
+			this.button12.Location = new System.Drawing.Point(81, 29);
 			this.button12.Margin = new System.Windows.Forms.Padding(2);
 			this.button12.Name = "button12";
 			this.button12.Size = new System.Drawing.Size(75, 23);
@@ -182,7 +184,7 @@
 			// button13
 			// 
 			this.button13.ForeColor = System.Drawing.Color.Purple;
-			this.button13.Location = new System.Drawing.Point(169, 38);
+			this.button13.Location = new System.Drawing.Point(160, 29);
 			this.button13.Margin = new System.Windows.Forms.Padding(2);
 			this.button13.Name = "button13";
 			this.button13.Size = new System.Drawing.Size(75, 23);
@@ -193,7 +195,7 @@
 			// button14
 			// 
 			this.button14.ForeColor = System.Drawing.Color.Black;
-			this.button14.Location = new System.Drawing.Point(248, 38);
+			this.button14.Location = new System.Drawing.Point(239, 29);
 			this.button14.Margin = new System.Windows.Forms.Padding(2);
 			this.button14.Name = "button14";
 			this.button14.Size = new System.Drawing.Size(75, 23);
@@ -204,7 +206,7 @@
 			// button15
 			// 
 			this.button15.ForeColor = System.Drawing.Color.Silver;
-			this.button15.Location = new System.Drawing.Point(327, 38);
+			this.button15.Location = new System.Drawing.Point(318, 29);
 			this.button15.Margin = new System.Windows.Forms.Padding(2);
 			this.button15.Name = "button15";
 			this.button15.Size = new System.Drawing.Size(75, 23);
@@ -215,7 +217,7 @@
 			// button16
 			// 
 			this.button16.ForeColor = System.Drawing.Color.Gray;
-			this.button16.Location = new System.Drawing.Point(406, 38);
+			this.button16.Location = new System.Drawing.Point(397, 29);
 			this.button16.Margin = new System.Windows.Forms.Padding(2);
 			this.button16.Name = "button16";
 			this.button16.Size = new System.Drawing.Size(75, 23);
@@ -226,7 +228,7 @@
 			// button17
 			// 
 			this.button17.ForeColor = System.Drawing.Color.White;
-			this.button17.Location = new System.Drawing.Point(485, 38);
+			this.button17.Location = new System.Drawing.Point(476, 29);
 			this.button17.Margin = new System.Windows.Forms.Padding(2);
 			this.button17.Name = "button17";
 			this.button17.Size = new System.Drawing.Size(75, 23);
@@ -234,31 +236,40 @@
 			this.button17.Text = "White";
 			this.button17.UseVisualStyleBackColor = true;
 			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.button1);
+			this.flowLayoutPanel1.Controls.Add(this.button2);
+			this.flowLayoutPanel1.Controls.Add(this.button3);
+			this.flowLayoutPanel1.Controls.Add(this.button4);
+			this.flowLayoutPanel1.Controls.Add(this.button5);
+			this.flowLayoutPanel1.Controls.Add(this.button6);
+			this.flowLayoutPanel1.Controls.Add(this.button7);
+			this.flowLayoutPanel1.Controls.Add(this.button8);
+			this.flowLayoutPanel1.Controls.Add(this.button9);
+			this.flowLayoutPanel1.Controls.Add(this.button10);
+			this.flowLayoutPanel1.Controls.Add(this.button11);
+			this.flowLayoutPanel1.Controls.Add(this.button12);
+			this.flowLayoutPanel1.Controls.Add(this.button13);
+			this.flowLayoutPanel1.Controls.Add(this.button14);
+			this.flowLayoutPanel1.Controls.Add(this.button15);
+			this.flowLayoutPanel1.Controls.Add(this.button16);
+			this.flowLayoutPanel1.Controls.Add(this.button17);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(795, 94);
+			this.flowLayoutPanel1.TabIndex = 17;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.Control;
-			this.ClientSize = new System.Drawing.Size(804, 81);
-			this.Controls.Add(this.button17);
-			this.Controls.Add(this.button16);
-			this.Controls.Add(this.button15);
-			this.Controls.Add(this.button14);
-			this.Controls.Add(this.button13);
-			this.Controls.Add(this.button12);
-			this.Controls.Add(this.button11);
-			this.Controls.Add(this.button10);
-			this.Controls.Add(this.button9);
-			this.Controls.Add(this.button8);
-			this.Controls.Add(this.button7);
-			this.Controls.Add(this.button6);
-			this.Controls.Add(this.button5);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(822, 118);
+			this.Controls.Add(this.flowLayoutPanel1);
 			this.Name = "Form1";
 			this.Text = "MainWindow";
+			this.flowLayoutPanel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -282,6 +293,7 @@
 		private System.Windows.Forms.Button button15;
 		private System.Windows.Forms.Button button16;
 		private System.Windows.Forms.Button button17;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 	}
 }
 
